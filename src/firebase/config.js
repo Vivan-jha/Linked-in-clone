4e96be99908577a58abfbcc1be47a35038ec0a36
Vivan-jha <92070374+Vivan-jha@users.noreply.github.com>
2022-11-02 02:43:31 +0530

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBNzbRGPLaHIU7ep8NB5ts1m98cP-cg_Hk",
+    authDomain: "linkedin-clone-457de.firebaseapp.com",
+    projectId: "linkedin-clone-457de",
+    storageBucket: "linkedin-clone-457de.appspot.com",
+    messagingSenderId: "154342496103",
+    appId: "1:154342496103:web:0e0e2c6f70060dd03881c2",
+    measurementId: "G-7CSYGC23PW"
+  };
+
+
+  export default firebaseConfig;
